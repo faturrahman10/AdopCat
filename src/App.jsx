0b1a9f454d138b6ee838cfa01effebb86b06661d
faurrahman10 <faturrahmanhasir@gmail.com>
2023-10-25 17:34:18 +0800

    membuat route baru

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/home';
 import Kucing from './pages/kucing';
+import Detail from './pages/detail';
+import AdminDashboard from './pages/admin/adminDashboard';
+import AdminAllData from './pages/admin/adminAllData';
+import AdminPost from './pages/admin/adminPost';
+
 
 function App() {
 
@@ -9,7 +14,10 @@ function App() {
       <Routes>
         <Route element={<Home/>} path="/"/>
         <Route element={<Kucing/>} path="/cat/list"/>
-        <Route element={<Kucing/>} path="/cat/detail/:id"/>
+        <Route element={<Detail />} path="/cat/detail/:id"/> 
+        <Route element={<AdminDashboard />} path="/admin/dashboard"/> 
+        <Route element={<AdminAllData />} path="/admin/data"/> 
+        <Route element={<AdminPost />} path="/admin/post"/> 
       </Routes>
   </BrowserRouter>
   )
