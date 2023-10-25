a1db55db3a49df4ab5cc74b2a8cba0d058f904ca
faurrahman10 <faturrahmanhasir@gmail.com>
2023-10-25 13:21:29 +0800

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,8 @@
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
       <nav className="px-20 h-16 flex justify-between items-center fixed top-0 right-0 left-0 z-50 bg-white">
@@ -7,17 +11,37 @@ const Navbar = () => {
         </div>
         <div>
           <ul className="flex justify-between gap-10">
-            <li className="text-lg font-semibold text-yellow-500 hover:text-yellow-600 cursor-pointer">
-              <a href="/">Home</a>
+            <li
+              className="text-lg font-semibold text-yellow-500 hover:text-yellow-600 cursor-pointer"
+              onClick={() => {
+                navigate("/");
+              }}
+              >
+              Home
             </li>
-            <li className="text-lg font-semibold text-yellow-500 hover:text-yellow-600 cursor-pointer">
-              <a href="/#aboutUs">About Us</a>
+            <li
+              className="text-lg font-semibold text-yellow-500 hover:text-yellow-600 cursor-pointer"
+              onClick={() => {
+                navigate("/#aboutUs");
+              }}
+            >
+              About Us
             </li>
-            <li className="text-lg font-semibold text-yellow-500 hover:text-yellow-600 cursor-pointer">
-              <a href="/cat/list">Adopsi</a>
+            <li
+              className="text-lg font-semibold text-yellow-500 hover:text-yellow-600 cursor-pointer"
+              onClick={() => {
+                navigate("/cat/list");
+              }}
+            >
+              Adopsi
             </li>
-            <li className="text-lg font-semibold text-yellow-500 hover:text-yellow-600 cursor-pointer">
-              <a href="/#QnA">QnA</a>
+            <li 
+              className="text-lg font-semibold text-yellow-500 hover:text-yellow-600 cursor-pointer"
+              onClick={() => {
+                navigate("/#QnA");
+              }}
+            >
+              QnA
             </li>
           </ul>
         </div>
