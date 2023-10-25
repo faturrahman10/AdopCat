8490a5ed95dc7462940c310bbc9fbcf391fbc034
faurrahman10 <faturrahmanhasir@gmail.com>
2023-10-25 11:12:07 +0800

    menambahkan route ke list kucing dan detail kucing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/home';
+import Kucing from './pages/kucing';
 
 function App() {
 
@@ -7,6 +8,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<Home/>} path="/"/>
+        <Route element={<Kucing/>} path="/cat/list"/>
+        <Route element={<Kucing/>} path="/cat/detail/:id"/>
       </Routes>
   </BrowserRouter>
   )
