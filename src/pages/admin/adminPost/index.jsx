f0008a93c6b98e2b2303e76e0e86ac8a3c174140
faurrahman10 <faturrahmanhasir@gmail.com>
2023-10-31 12:03:31 +0800

    memperbarui footer

--- src/pages/admin/adminPost/index.jsx
+++ src/pages/admin/adminPost/index.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import AdminNavbar from '../../../components/AdminNavbar';
+import Sidebar from '../../../components/Sidebar';
 
 const AdminPost = () => {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     name: '',
     age: '',
@@ -34,87 +36,94 @@ const AdminPost = () => {
     } catch (error) {
       console.error('Terjadi kesalahan:', error);
     }
+    navigate("/admin/data")
   };
 
   return (
     <div>
-      <AdminNavbar/>
-      <div className="pl-[20%]">
+      <Sidebar/>
+      <div className="pl-[20%] pt-5 pr-10">
         <h1 className="text-3xl font-semibold mb-4">Post Data</h1>
-        <div className="max-w-md">
-          <div className="mb-4">
-            <label htmlFor="name" className="block font-semibold mb-1">Name</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleInputChange}
-              className="w-full h-10 p-2 border border-gray-300 rounded focus:outline-none"
-            />
+        <div className="flex justify-center">
+          <div className="w-[80%]">
+            <div className="flex gap-4">
+              <div className="mb-4 w-1/2">
+                <label htmlFor="name" className="block font-semibold mb-1">Name</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleInputChange}
+                  className="w-full h-10 p-2 border border-gray-300 rounded focus:outline-none"
+                />
+              </div>
+              <div className="mb-4 w-1/2">
+                <label htmlFor="age" className="block font-semibold mb-1">Age</label>
+                <input
+                  type="text"
+                  id="age"
+                  name="age"
+                  value={formData.age}
+                  onChange={handleInputChange}
+                  className="w-full h-10 p-2 border border-gray-300 rounded focus:outline-none"
+                />
+              </div>
+            </div>
+            <div className="flex gap-4">
+              <div className="mb-4 w-1/2">
+                <label htmlFor="gender" className="block font-semibold mb-1">Gender</label>
+                <input
+                  type="text"
+                  id="gender"
+                  name="gender"
+                  value={formData.gender}
+                  onChange={handleInputChange}
+                  className="w-full h-10 p-2 border border-gray-300 rounded focus:outline-none"
+                />
+              </div>
+              <div className="mb-4 w-1/2">
+                <label htmlFor="imgURL" className="block font-semibold mb-1">Image URL</label>
+                <input
+                  type="text"
+                  id="imgURL"
+                  name="imgURL"
+                  value={formData.imgURL}
+                  onChange={handleInputChange}
+                  className="w-full h-10 p-2 border border-gray-300 rounded focus:outline-none"
+                />
+              </div>
+            </div>
+            <div className="mb-4">
+              <label htmlFor="healthCondition" className="block font-semibold mb-1">Health Condition</label>
+              <textarea
+                type="text"
+                id="healthCondition"
+                name="healthCondition"
+                value={formData.healthCondition}
+                onChange={handleInputChange}
+                className="w-full p-2 border border-gray-300 rounded focus:outline-none"
+                rows="4"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="description" className="block font-semibold mb-1">Description</label>
+              <textarea
+                id="description"
+                name="description"
+                value={formData.description}
+                onChange={handleInputChange}
+                className="w-full p-2 border border-gray-300 rounded focus:outline-none"
+                rows="4"
+              />
+            </div>
+            <button
+              onClick={handleSubmit}
+              className="bg-yellow-500 hover:bg-yellow-600 px-4 py-2 rounded-lg text-white font-semibold"
+            >
+              Kirim Data
+            </button>
           </div>
-          <div className="mb-4">
-            <label htmlFor="age" className="block font-semibold mb-1">Age</label>
-            <input
-              type="text"
-              id="age"
-              name="age"
-              value={formData.age}
-              onChange={handleInputChange}
-              className="w-full h-10 p-2 border border-gray-300 rounded focus:outline-none"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="gender" className="block font-semibold mb-1">Gender</label>
-            <input
-              type="text"
-              id="gender"
-              name="gender"
-              value={formData.gender}
-              onChange={handleInputChange}
-              className="w-full h-10 p-2 border border-gray-300 rounded focus:outline-none"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="imgURL" className="block font-semibold mb-1">Image URL</label>
-            <input
-              type="text"
-              id="imgURL"
-              name="imgURL"
-              value={formData.imgURL}
-              onChange={handleInputChange}
-              className="w-full h-10 p-2 border border-gray-300 rounded focus:outline-none"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="healthCondition" className="block font-semibold mb-1">Health Condition</label>
-            <textarea
-              type="text"
-              id="healthCondition"
-              name="healthCondition"
-              value={formData.healthCondition}
-              onChange={handleInputChange}
-              className="w-full p-2 border border-gray-300 rounded focus:outline-none"
-              rows="4"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="description" className="block font-semibold mb-1">Description</label>
-            <textarea
-              id="description"
-              name="description"
-              value={formData.description}
-              onChange={handleInputChange}
-              className="w-full p-2 border border-gray-300 rounded focus:outline-none"
-              rows="4"
-            />
-          </div>
-          <button
-            onClick={handleSubmit}
-            className="bg-yellow-500 hover:bg-yellow-600 px-4 py-2 rounded-xl text-white font-semibold"
-          >
-            Post Data
-          </button>
         </div>
       </div>
     </div>
