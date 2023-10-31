d4e0cd87d11c56d9451498a9fc8cd53e942f45e4
faurrahman10 <faturrahmanhasir@gmail.com>
2023-10-31 12:03:45 +0800

    memperbarui footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 const Footer = () => {
   return (
     <div>
-      <div className="bg-yellow-500 flex gap-36 px-[5%] py-[3%]">
+      <div className="bg-yellow-500 flex flex-wrap gap-16 px-[5%] py-[3%]">
         <div>
           <a href="/" className="font-semibold text-3xl">adop<span className="text-white">cat</span></a>
         </div>
         <div>
-          <h2 className="font-semibold text-xl text-white">Information Content</h2>
+          <h2 className="font-semibold text-xl text-white mb-3">Information Content</h2>
           <div>
             <ul>
               <li className="text-white">
@@ -22,16 +25,28 @@ const Footer = () => {
           </div>
         </div>
         <div>
-          <h2 className="font-semibold text-xl text-white">Social Media</h2>
+          <h2 className="font-semibold text-xl text-white mb-3">Social Media</h2>
           <div>
-            <ul>
-              <li></li>
-              <li></li>
+            <ul className="flex gap-3">
+              <li>
+                <FontAwesomeIcon
+                  size="xl"
+                  className="text-white"
+                  icon={faFacebook}
+                />
+              </li>
+              <li>
+                <FontAwesomeIcon
+                  size="xl"
+                  className="text-white"
+                  icon={faInstagram}
+                />
+              </li>
             </ul>
           </div>
         </div>
         <div>
-          <h2 className="font-semibold text-xl text-white">Contact</h2>
+          <h2 className="font-semibold text-xl text-white mb-3">Contact</h2>
           <div>
             <ul>
               <li className="text-white"><span className="font-semibold">Phone</span> : +62 821 1010 1010</li>
