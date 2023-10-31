7f4d16909776897eac7eff5bb517135693b99bde
faurrahman10 <faturrahmanhasir@gmail.com>
2023-10-31 12:02:05 +0800

    menambahkan route baru

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/home';
 import Kucing from './pages/kucing';
 import Detail from './pages/detail';
-import AdminDashboard from './pages/admin/adminDashboard';
 import AdminAllData from './pages/admin/adminAllData';
 import AdminPost from './pages/admin/adminPost';
-
+import ChatBot from './pages/chatBot';
+import History from './pages/history';
+import LoginUser from './pages/loginUser';
 
 function App() {
 
@@ -13,9 +14,11 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<Home/>} path="/"/>
+        <Route element={<LoginUser/>} path="/auth/loginUser"/>
         <Route element={<Kucing/>} path="/cat/list"/>
-        <Route element={<Detail />} path="/cat/detail/:id"/> 
-        <Route element={<AdminDashboard />} path="/admin/dashboard"/> 
+        <Route element={<Detail />} path="/cat/detail/:id"/>
+        <Route element={<ChatBot />} path="/cat/chatBot"/>
+        <Route element={<History />} path="/cat/history"/>
         <Route element={<AdminAllData />} path="/admin/data"/> 
         <Route element={<AdminPost />} path="/admin/post"/> 
       </Routes>
