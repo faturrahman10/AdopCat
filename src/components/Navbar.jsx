4148097f52e5795c852c9c37a1a3087703456798
faurrahman10 <faturrahmanhasir@gmail.com>
2023-10-31 12:02:44 +0800

    membperbarui navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,62 @@
 import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
+import Swal from "sweetalert2";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const username = Cookies.get("email");
+
+  const handleAuth = () => {
+    if (username) {
+      Swal.fire({
+        text: "LogOut sekarang?",
+        title: "Logout",
+        confirmButtonText: "OK",
+      }).then((res) => {
+        if (res.isConfirmed) {
+          Cookies.remove("email");
+          Cookies.remove("token");
+          navigate("/auth/loginUser");
+        }
+      });
+    } else {
+      Swal.fire({
+        text: "LogIn Sekarang?",
+        title: "Login",
+        confirmButtonText: "OK",
+      }).then((res) => {
+        if (res.isConfirmed) {
+          navigate("/auth/loginUser")
+        }
+      });
+    }
+  };
+  
+
+  const handleHistoryClick = () => {
+    if (username) {
+      navigate("/cat/history");
+    } else {
+      Swal.fire({
+        text: "Anda harus login terlebih dahulu",
+        title: "Login Required",
+        icon: "warning",
+        confirmButtonText: "OK",
+      });
+    }
+  };
+  
 
   return (
     <div>
-      <nav className="px-20 h-16 flex justify-between items-center fixed top-0 right-0 left-0 z-50 bg-white">
+      <nav className="px-20 h-16 flex justify-between items-center fixed top-0 right-0 left-0 z-20 bg-white shadow-md">
         <div>
           <a href="/" className="font-semibold text-3xl">adop<span className="text-yellow-500">cat</span></a>
         </div>
         <div>
-          <ul className="flex justify-between gap-10">
+          <ul className="flex justify-between items-center43 gap-10">
             <li
-              className="text-lg font-semibold text-yellow-500 hover:text-yellow-600 cursor-pointer"
+              className="text-lg font-semibold text-slate-500 hover:text-yellow-600 cursor-pointer"
               onClick={() => {
                 navigate("/");
               }}
@@ -20,15 +64,7 @@ const Navbar = () => {
               Home
             </li>
             <li
-              className="text-lg font-semibold text-yellow-500 hover:text-yellow-600 cursor-pointer"
-              onClick={() => {
-                navigate("/#aboutUs");
-              }}
-            >
-              About Us
-            </li>
-            <li
-              className="text-lg font-semibold text-yellow-500 hover:text-yellow-600 cursor-pointer"
+              className="text-lg font-semibold text-slate-500 hover:text-yellow-600 cursor-pointer"
               onClick={() => {
                 navigate("/cat/list");
               }}
@@ -36,14 +72,28 @@ const Navbar = () => {
               Adopsi
             </li>
             <li 
-              className="text-lg font-semibold text-yellow-500 hover:text-yellow-600 cursor-pointer"
+              className="text-lg font-semibold text-slate-500 hover:text-yellow-600 cursor-pointer"
               onClick={() => {
-                navigate("/#QnA");
+                navigate("/cat/chatBot");
               }}
             >
               QnA
             </li>
+            <li 
+              className="text-lg font-semibold text-slate-500 hover:text-yellow-600 cursor-pointer"
+              onClick={handleHistoryClick}
+            >
+              History
+            </li>
           </ul>
+        </div>
+        <div>
+          <div
+            className="px-3 py-1 bg-yellow-500 hover:bg-yellow-500 rounded-md text-white font-semibold cursor-pointer"
+            onClick={() => handleAuth()}
+          >
+            {username ? "LogOut" : "LogIn"}  
+          </div>
         </div>
       </nav>
     </div>
